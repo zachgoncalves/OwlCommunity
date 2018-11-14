93ecdc33a2063cebb05619722f23ed7117a2362b
Zach Goncalves <zach.j.goncalves@gmail.com>
2018-11-14 16:27:07 -0500

    Implement OwlMemberList clas

--- OwlCommunity/Classes/OwlMemberList.cs
+++ OwlCommunity/Classes/OwlMemberList.cs
@@ -3,11 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OwlCommunity.Models;
 
 namespace OwlCommunity.Classes
 {
     public class OwlMemberList
     {
+        List<OwlMember> memberList;
+        int numMembers;
+        string owlMember; 
+
+        // Parameterless Constructor
+        public OwlMemberList()
+        {
+            numMembers = 0;
+            memberList = new List<OwlMember>();
+        }
+
+        // Returns number of members in the member list
+        public int getNumMembers()
+        {
+            return numMembers = memberList.Count();
+        }
+
+        // Adds member to list
+        public void addToList(OwlMember member)
+        {
+            memberList.Add(member);
+        }
+
+        // Removes member from list
+        public void removeFromList(OwlMember member)
+        {
+            memberList.Remove(member);
+        }
+
+        // Searches for OwlMember in the list
+        public string searchList(string keyword, ref bool searchSuccessful)
+        {
+            searchSuccessful = false;
+            string returnMember = "";
+            foreach (OwlMember member in memberList)
+            {
+                bool match = false;
+                bool endOfList = false;
+              
+                if (match)
+                {
+                    returnMember = member.ToString();
+                    searchSuccessful = true;
+                    break;
+                }
+                else
+                {
+                    if (endOfList)
+                    {
+                        returnMember = "No member found!";
+                        searchSuccessful = false;
+                    }
+                }
+            }
+            return returnMember;
+        }
 
     }
 }
