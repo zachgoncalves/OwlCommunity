f6b18ed7d7ee7be7db59ff83b434210a29c006b5
Zach Goncalves <zach.j.goncalves@gmail.com>
2018-12-01 19:50:27 -0500

    Begin Phase 2 of project - implementation of database

---
+++ OwlCommunity/Classes/OwlMemberDB.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwlCommunity.Classes
+{
+    public class OwlMemberDB
+    {
+
+        public OleDbDataReader SelectOwlMemberFromFaculty(int OwlMemberID, out bool OKFlag)
+        {
+            string strSelectOwlMember = "SELECT OWLMEMBER.fldID, OWLMEMBER.fldName, OWLMEMBER.fldBirthdate, "
+                    + "FACULTY.fldDepartment, FACULTY.fldRank FROM OWLMEMBER "
+                    + "INNER JOIN FACULTY ON FACULTY.fldID = OWLMEMBER.fldID "
+                    + "WHERE FACULTY.fldID = " + OwlMemberID + ";";
+
+            OleDbConnection myConnection = new OleDbConnection(strConnection);
+            OleDbCommand myCommand = new OleDbCommand(strSelectOwlMember, myConnection);
+            OleDbDataReader myDataReader;
+
+            try
+            {
+                myConnection.Open();
+                myDataReader = myCommand.ExecuteReader();
+                OKFlag = true; // returns true if Select was successful
+            }
+            catch (OleDbException ex)
+            {
+                Console.Write("There was a Select Faculty member error: " + ex.Message);
+                myDataReader = null;
+                myConnection.Close();
+                OKFlag = false; // returns false if Select was unsuccessful
+            }
+
+            return myDataReader;
+        }  // end SelectOwlMemberFromFaculty
+
+
+        public OleDbDataReader SelectOwlMemberFromGraduateStudent(int OwlMemberID, out bool OKFlag)
+        {
+            string strSelectOwlMember = "SELECT OWLMEMBER.fldID, OWLMEMBER.fldName, OWLMEMBER.fldBirthdate, " + "STUDENT.fldMajor, STUDENT.fldGPA, "
+                + "GRADUATESTUDENT.fldStipend, GRADUATESTUDENT.fldDegreeProgram FROM (OWLMEMBER "
+                + "INNER JOIN STUDENT ON STUDENT.fldID = OWLMEMBER.fldID) "
+                + "INNER JOIN GRADUATESTUDENT ON GRADUATESTUDENT.fldID = OWLMEMBER.fldID "
+                + "WHERE OWLMEMBER.fldID = " + OwlMemberID + ";";
+
+            OleDbConnection myConnection = new OleDbConnection(strConnection);
+            OleDbCommand myCommand = new OleDbCommand(strSelectOwlMember, myConnection);
+            OleDbDataReader myDataReader;
+
+            try
+            {
+                myConnection.Open();
+                myDataReader = myCommand.ExecuteReader();
+                OKFlag = true;
+            }
+            catch (OleDbException ex)
+            {
+                Console.Write("There was a Select Graduate Student error: " + ex.Message);
+                myConnection.Close();
+                myDataReader = null;
+                OKFlag = false; // returns false if Select was unsuccessful
+            }
+
+            return myDataReader;
+        } // end SelectOwlMemberFromGraduateStudent
+
+    }
+}
---
+++ create-tables-script.sql
@@ -0,0 +1,41 @@
+﻿CREATE TABLE owl_members (
+    [ID]        INT           NOT NULL,
+    [Full_Name] VARCHAR (255) NOT NULL,
+    [Birthdate] DATE          NOT NULL, 
+    CONSTRAINT [PK_owl_members] PRIMARY KEY ([ID])
+);
+CREATE TABLE students
+(
+	[ID] INT NOT NULL,
+    [Major] VARCHAR(255) NOT NULL, 
+    [GPA] NUMERIC NOT NULL,
+	CONSTRAINT [PK_student] PRIMARY KEY ([ID])
+);
+CREATE TABLE undergraduate_students 
+(
+	[ID] INT NOT NULL,
+	[Tuition] DECIMAL NOT NULL,
+	[Credits] INT NOT NULL,
+	[Year] VARCHAR(255),
+	CONSTRAINT [PK_undergraduate_student] PRIMARY KEY ([ID])
+);
+CREATE TABLE graduate_students
+(
+	[ID] INT NOT NULL,
+	[Stipend] DECIMAL NOT NULL,
+	[Degree_Program] VARCHAR(255) NOT NULL,
+	CONSTRAINT [PK_graduate_student] PRIMARY KEY ([ID])
+);
+CREATE TABLE faculty 
+(
+	[ID] INT NOT NULL,
+	[Department] VARCHAR(255) NOT NULL,
+	[Rank] VARCHAR(255) NOT NULL,
+	CONSTRAINT [PK_faculty] PRIMARY KEY ([ID])
+);
+CREATE TABLE faculty_chair 
+(
+	[ID] INT NOT NULL,
+	[Stipend] DECIMAL NOT NULL,
+	CONSTRAINT [PK_faculty_chair] PRIMARY KEY ([ID])
+);
